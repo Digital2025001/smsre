34abfa49bc218967922cb54a1bfa082727a41983
Digital2025001 <bosmichael30@gmail.com>
2026-01-18 13:51:28 -0800

    Update nbbank.ts

--- nbbank.ts
+++ nbbank.ts
@@ -4,7 +4,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://ssapar.vercel.app/", // Replace this with your actual link
+      Location: "https://poemcellars.com/", // Replace this with your actual link
     },
   });
 });
